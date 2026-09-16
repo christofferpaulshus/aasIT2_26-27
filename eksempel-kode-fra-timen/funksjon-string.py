4cdee35ff86307b2a001af8a0ce011281c03d4bd
Dru C. Romano Abello <druabello@gmail.com>
2026-09-16 08:59:39 +0200

    refactor: change variable names

--- eksempel-kode-fra-timen/funksjon-string.py
+++ eksempel-kode-fra-timen/funksjon-string.py
@@ -1,18 +1,19 @@
 # capitalize string
 # if input is multi-word, then return a list over all words
-def f(text: str):
+def f(input_string: str):
 	array = []
 	word = ""
-	capitalized_text = text.upper();
+	text = input_string.upper();
 
-	for char in range(len(capitalized_text)):
-		if capitalized_text[char] == ' ': 
+	for i in range(len(text)):
+		if text[i] == ' ': 
 			array.append(word)
 			word = ""
-		elif char == len(capitalized_text) - 1:
-			word += capitalized_text[char]
+		elif i == len(text) - 1:
+			word += text[i]
 			array.append(word)
-		else: word += capitalized_text[char]
+		else: 
+			word += text[i]
 
 	return array
 
